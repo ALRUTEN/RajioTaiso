cfc426a27643d5286222af422a9601492eeaaa0c
ALRUTEN <laurentnavarro31@gmail.com>
2017-10-05 19:16:08 +0200

    execice3

--- app/src/main/java/com/example/apprenti/rajiotaiso/MainActivity.java
+++ app/src/main/java/com/example/apprenti/rajiotaiso/MainActivity.java
@@ -2,12 +2,29 @@ package com.example.apprenti.rajiotaiso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button add_user;
+    EditText username_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        add_user = (Button) findViewById(R.id.add_user);
+        username_input = (EditText) findViewById(R.id.username_input);
+
+        add_user.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,(username_input.getText().toString()),Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
